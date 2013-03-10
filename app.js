aa9ff1413f6d102c9074663ce2336c494aa59955
rjsteinert <rj@rjsteinert.com>
2013-03-10 22:26:35 +0000

    Stash.

--- app.js
+++ app.js
@@ -7,8 +7,41 @@ var _ = require("underscore")
 // For Express 3
 app.use(express.bodyParser())
 
-app.post('/:path/:filename', function(req, res){
-  fs.writeFile(req.params.path + '/' + req.params.filename, JSON.stringify(req.body, null, 4))
+var library = "http://raspberrypi.local:5984"
+var traildb = "/User/rj/Box/OWL/data"
+
+app.post('/addCouchResourceToGroup/:groupId/:couchId', function(req, res){
+  $.getJSON(library + "/library/" + req.params.couchId, function(resource) {
+    var topics = fs.openFile(traildb + "/groups/" + req.params.groupId + "/topics")
+    // Build the path
+    var map = resource.path.split("/")
+    var breadcrumb = ""
+    var trail =& topics
+    $.each(map, function(key, cairn) {
+      breadcrumb += cairn + "/"
+      // Assume we're going to have to blaze the trail unless later proven wrong
+      var blazeTrail = true
+      var cairnLocation = null
+      $.each(trail.children, function(key, child) {
+        if(child.path == breadcrumb) {
+          cairnLocation = key
+        }
+      })
+      if (cairnLocation === null) {
+        // blaze the trail
+        if(!_.hasProperty(trail, "children"))
+          trail.children = []
+        trail.children 
+
+      }
+
+
+      trail =& 
+
+    })
+    fs.writeFile(traildb + "/groups/" + req.params.groupId + "/topics", JSON.stringify(topics, null, 4))
+
+  })
   res.send('success')
 })
 app.listen(4200)
