4717db72207c4c89e5d5f7da96f42fb3e57675ac
rjsteinert <rj@rjsteinert.com>
2013-03-13 13:54:06 +0000

    Patch failing to apply when the tree isn't fully built for the resource being added. Going to have to take a recursive approach that builds the path as a function calls itself.

--- app.js
+++ app.js
@@ -1,48 +1,19 @@
 var express = require('express')
 var app = express()
 var request = require('request')
-var fs = require('fs-extra')
+var fs = require('fs')
 var $ = require('jQuery')
 var _ = require("underscore")
 var jsonpatch = require("json-patch")
 // For Express 3
 app.use(express.bodyParser())
 
-var library = "http://raspberrypi.local:5984"
-var traildb = "/User/rj/Box/OWL/data"
+var library = "http://192.168.0.101:5984/library"
+var dataFolder = "/Users/rj/Box/OWL/data"
 
 app.post('/addCouchResourceToGroup/:groupId/:couchId', function(req, res){
-  $.getJSON(library + "/library/" + req.params.couchId, function(resource) {
-    
-    var topics = fs.openFile(traildb + "/groups/" + req.params.groupId + "/topics")
-    
-    var jsonPath = translateKatPathToJsonPath(resource.path, topics)
-    
-    var jsonPathArray = jsonPath.split("/")
-    jsonPathArray.pop()
-    jsonPathArray.shift()
-
-    var resourcePathArray = resource.path.split("/")
-    resourcePathArray.pop()
-    resourcePathArray.shift()
-
-    var fillInTopicInfo = false
-    if(resourcePathArray.count() != (jsonPathArray.count() / 2)) {
-      fillInTopicInfo = true
-      // Blaze the trail, fill in topic info later
-      var pathAhead = resourcePathArray.shift((jsonPathArray.count() / 2))
-      $.each(pathAhead, function(key, value) {
-        jsonPath += "0/children/"
-      })
-    }
-
-    jsonpath.apply(topics, [{op: 'add', path: jsonPath + "0", value: resource}])
-
-    fs.writeFile(traildb + "/groups/" + req.params.groupId + "/topics", JSON.stringify(topics, null, 4))
-    if(fillInTopicInfo == true) {
-      fixTopicInfo(req.params.groupId, jsonPath)
-    }
-
+  $.getJSON(library + "/" + req.params.couchId, function(resource) {
+    addResourceToGroup(req.params.groupId, resource)
   })
   res.send('success')
 })
@@ -53,10 +24,47 @@ var fixTopicInfo = function (groupId, jsonPath) {
   // @todo
 }
 
+var addResourceToGroup = function(groupId, resource) {
+  var topicsFilePath = dataFolder + "/groups/" + groupId + "/topics"
+  fs.readFile(topicsFilePath, "utf8", function(err, topicsJson) {
+    
+    var topics = JSON.parse(topicsJson)
+    
+    var jsonPath = translateKatPathToJsonPath(resource.path, topics)
+    var jsonPathArray = jsonPath.split("/")
+    jsonPathArray.pop()
+    jsonPathArray.shift()
+    console.log(jsonPathArray)
+
+    var resourcePathArray = resource.path.split("/")
+    resourcePathArray.pop()
+    resourcePathArray.shift()
+    console.log(resourcePathArray)
+
+    var fillInTopicInfo = false
+    if(resourcePathArray.length != (jsonPathArray.length / 2)) {
+      fillInTopicInfo = true
+      // Blaze the trail, fill in topic info later
+      var pathAhead = resourcePathArray.shift((jsonPathArray.length / 2))
+      $.each(pathAhead, function(key, value) {
+        jsonPath += "0/children/"
+      })
+    }
+
+    jsonpatch.apply(JSON.stringify(topics), [{op: 'add', path: jsonPath + "0", value: resource}])
+
+    fs.writeFile(dataFolder + "/groups/" + req.params.groupId + "/topics", JSON.stringify(topics, null, 4))
+    if(fillInTopicInfo == true) {
+      fixTopicInfo(req.params.groupId, jsonPath)
+    }
+  })
+  
+}
+
 /*
  * Translate a path attribute found in KA Lite Topics.json to the JSON path of that object
  */
-var translateKatPathToJsonPath = function(path, object) {
+var translateKatPathToJsonPath = function(path, topics) {
   // The slugs we'll be looking for
   var slugs = path.split("/")
   // Get rid of unneccessary blanks at end and beginning of array
@@ -67,8 +75,8 @@ var translateKatPathToJsonPath = function(path, object) {
   var map = []
 
   $.each(slugs, function(slugKey, slug) {
-    if(object.hasOwnProperty("children")) { 
-      $.each(object.children, function(mapKey, child) {
+    if(_.has(topics, "children")) { 
+      $.each(topics.children, function(mapKey, child) {
         if(child.slug == slug){
           map[slugKey] = mapKey
           object = child
